39941ef15f5ea7c9b20aeb7160184f15dd5dfa3b
Anish Sinha <anishsinha0128@gmail.com>
2021-08-20 18:14:54 -0400

    Initial commit

---
+++ src/MergeSort.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MergeSort {
+    //Main method
+    public static void main(String[] args) {
+        ArrayList<Double> numbers = new ArrayList<Double>();
+        String filePath = "resources/numbers.csv";
+        String line = "";
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while((line = reader.readLine()) != null){ 
+                numbers.add(Double.parseDouble(line.split(",")[0]));
+            }
+            reader.close();
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        mergeSort(numbers);
+        for(int i=0; i<numbers.size();i++) System.out.println(numbers.get(i));
+    }
+    
+    public static void mergeSort(ArrayList<Double> originalArray){
+        int arraySize = originalArray.size();
+        if(arraySize<2) return;
+        int mid = (int)Math.floor(arraySize/2);
+        ArrayList<Double> left = new ArrayList<Double>(mid);
+        ArrayList<Double> right = new ArrayList<Double>(arraySize-mid);
+        for(int i=0; i<mid; i++){
+            left.add(originalArray.get(i));
+        }
+        for(int j=mid; j<arraySize; j++){
+            right.add(originalArray.get(j));
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, originalArray);
+
+    }
+
+    //Merge method
+    public static ArrayList<Double> merge(ArrayList<Double> left, ArrayList<Double> right, ArrayList<Double> unsortedNumbers){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < left.size() && j < right.size()){
+            if(left.get(i) < right.get(j)){
+                unsortedNumbers.set(k, left.get(i));
+                i++;
+            }else {
+                unsortedNumbers.set(k, right.get(j));
+                j++;
+            }
+            k++;
+        }
+        while(i<left.size()){
+            unsortedNumbers.set(k, left.get(i));
+            i++;
+            k++;
+        }
+        while(j<right.size()){
+            unsortedNumbers.set(k, right.get(j));
+            j++;
+            k++;
+        }
+        return unsortedNumbers;
+    }
+}
