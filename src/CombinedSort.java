01d6025522b4138453da106f4b1a362c34d255aa
Anish Sinha <anishsinha0128@gmail.com>
2021-08-21 10:03:40 -0400

    Added combined algorithm

---
+++ src/CombinedSort.java
@@ -0,0 +1,118 @@
+//* Extremely efficient sorting algorithm.
+//* Performs mergeSort on large n but insertionSort on small n where it is more efficient.
+//* This algorithm is approximately twice as a fast as a plain mergeSort algorithm.
+//* Over a test of 250 trials of sorting 1000 numbers between 0 and 100000, this algorithm 
+//* took approximately 0.328 seconds. In comparison, plain mergeSort took about 0.7 seconds. 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CombinedSort {
+    // constants
+    public static final int THRESHOLD = 43; // Threshold for which insertionSort beats mergeSort
+    // main method
+
+    public static void main(String[] args) {
+        ArrayList<Double> list = new ArrayList<Double>();
+        ArrayList<Double> times = new ArrayList<Double>();
+        String filePath = "resources/largeDataset.csv";
+        String line = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) != null) {
+                list.add(Double.parseDouble(line.split(",")[0]));
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        // time function
+        for (int i = 0; i < 250; i++) {
+            double startTime = System.currentTimeMillis();
+            mergeSort(list);
+            double endTime = System.currentTimeMillis();
+            times.add(endTime - startTime);
+        }
+        Double averageTime = times.stream().mapToDouble(d -> d).average().orElse(0.0);
+        System.out.println(averageTime);
+    }
+
+    // merge
+    public static ArrayList<Double> merge(ArrayList<Double> left, ArrayList<Double> right,
+            ArrayList<Double> unsortedNumbers) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i) < right.get(j)) {
+                unsortedNumbers.set(k, left.get(i));
+                i++;
+            } else {
+                unsortedNumbers.set(k, right.get(j));
+                j++;
+            }
+            k++;
+        }
+        while (i < left.size()) {
+            unsortedNumbers.set(k, left.get(i));
+            i++;
+            k++;
+        }
+        while (j < right.size()) {
+            unsortedNumbers.set(k, right.get(j));
+            j++;
+            k++;
+        }
+        return unsortedNumbers;
+    }
+
+    // mergeSort
+    public static void mergeSort(ArrayList<Double> originalArray) {
+        int arraySize = originalArray.size();
+        if (arraySize < THRESHOLD)
+            insertionSort(originalArray);
+        else {
+            int mid = (int) Math.floor(arraySize / 2);
+            ArrayList<Double> left = new ArrayList<Double>(mid);
+            ArrayList<Double> right = new ArrayList<Double>(arraySize - mid);
+            for (int i = 0; i < mid; i++) {
+                left.add(originalArray.get(i));
+            }
+            for (int j = mid; j < arraySize; j++) {
+                right.add(originalArray.get(j));
+            }
+            mergeSort(left);
+            mergeSort(right);
+            merge(left, right, originalArray);
+        }
+    }
+
+    // insertionSort
+    public static ArrayList<Double> insertionSort(ArrayList<Double> array) {
+        int arrayLength = array.size();
+        for (int i = 0; i < arrayLength; i++) {
+            Double key = array.get(i);
+            int j = i - 1;
+            while (j >= 0 && array.get(j) > key) {
+                array.set(j + 1, array.get(j));
+                j--;
+            }
+            array.set(j + 1, key);
+        }
+        return array;
+    }
+
+    // print
+    public static void print(ArrayList<Double> array) {
+        int arrayLength = array.size();
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println(array.get(i));
+        }
+    }
+}
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -5,37 +5,45 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MergeSort {
-    //Main method
+    // Main method
     public static void main(String[] args) {
         ArrayList<Double> numbers = new ArrayList<Double>();
+        ArrayList<Double> times = new ArrayList<Double>();
         String filePath = "resources/numbers.csv";
         String line = "";
-        try{
+        try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            while((line = reader.readLine()) != null){ 
+            while ((line = reader.readLine()) != null) {
                 numbers.add(Double.parseDouble(line.split(",")[0]));
             }
             reader.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e);
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
-        mergeSort(numbers);
-        for(int i=0; i<numbers.size();i++) System.out.println(numbers.get(i));
+        for (int i = 0; i < 250; i++) {
+            double startTime = System.currentTimeMillis();
+            mergeSort(numbers);
+            double endTime = System.currentTimeMillis();
+            times.add(endTime - startTime);
+        }
+        Double averageTime = times.stream().mapToDouble(d -> d).average().orElse(0.0);
+        System.out.println(averageTime);
     }
-    
-    //MergeSort method
-    public static void mergeSort(ArrayList<Double> originalArray){
+
+    // MergeSort method
+    public static void mergeSort(ArrayList<Double> originalArray) {
         int arraySize = originalArray.size();
-        if(arraySize<2) return;
-        int mid = (int)Math.floor(arraySize/2);
+        if (arraySize < 2)
+            return;
+        int mid = (int) Math.floor(arraySize / 2);
         ArrayList<Double> left = new ArrayList<Double>(mid);
-        ArrayList<Double> right = new ArrayList<Double>(arraySize-mid);
-        for(int i=0; i<mid; i++){
+        ArrayList<Double> right = new ArrayList<Double>(arraySize - mid);
+        for (int i = 0; i < mid; i++) {
             left.add(originalArray.get(i));
         }
-        for(int j=mid; j<arraySize; j++){
+        for (int j = mid; j < arraySize; j++) {
             right.add(originalArray.get(j));
         }
         mergeSort(left);
@@ -44,27 +52,28 @@ public class MergeSort {
 
     }
 
-    //Merge method
-    public static ArrayList<Double> merge(ArrayList<Double> left, ArrayList<Double> right, ArrayList<Double> unsortedNumbers){
+    // Merge method
+    public static ArrayList<Double> merge(ArrayList<Double> left, ArrayList<Double> right,
+            ArrayList<Double> unsortedNumbers) {
         int i = 0;
         int j = 0;
         int k = 0;
-        while(i < left.size() && j < right.size()){
-            if(left.get(i) < right.get(j)){
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i) < right.get(j)) {
                 unsortedNumbers.set(k, left.get(i));
                 i++;
-            }else {
+            } else {
                 unsortedNumbers.set(k, right.get(j));
                 j++;
             }
             k++;
         }
-        while(i<left.size()){
+        while (i < left.size()) {
             unsortedNumbers.set(k, left.get(i));
             i++;
             k++;
         }
-        while(j<right.size()){
+        while (j < right.size()) {
             unsortedNumbers.set(k, right.get(j));
             j++;
             k++;
