fce0bc85797d0a59e94c3e29eff63ac11a28ee32
Anish Sinha <anishsinha0128@gmail.com>
2021-08-23 15:34:24 -0400

    finished polynomial algorithms

--- src/HornersRule.java
+++ src/HornersRule.java
@@ -1,16 +1,26 @@
 
+//This algorithm is faster than the naive polynomial evaluation algorithm but the test polynomials are too small to be able to tell
+import java.util.ArrayList;
+
 public class HornersRule {
     public static void main(String[] args) {
-        int[] poly = { -1, 2, -6, 2 };
         int x = 3;
+        double[] poly = { -1, 2, -6, 2 };
         int n = poly.length;
-        System.out.printf("The value of the polynomial at x=%d is %d", x, horner(poly, n, x));
-
+        ArrayList<Double> times = new ArrayList<Double>();
+        for (int i = 0; i < 1000; i++) {
+            double startTime = System.currentTimeMillis();
+            horner(poly, n, x);
+            double endTime = System.currentTimeMillis();
+            times.add(endTime - startTime);
+        }
+        Double averageTime = times.stream().mapToDouble(d -> d).average().orElse(0.0);
+        System.out.println("Average time: " + averageTime);
     }
 
     // Polynomial evaluation
-    public static int horner(int poly[], int n, int x) {
-        int result = 0;
+    public static double horner(double poly[], int n, int x) {
+        double result = 0;
         for (int i = poly.length; i > 0; i--)
             result = poly[i - 1] + result * x;
         return result;
--- src/NaivePolynomial.java
+++ src/NaivePolynomial.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class NaivePolynomial {
     public static void main(String[] args) {
+        double[] poly = { -1, 2, -6, 2 };
+        ArrayList<Double> times = new ArrayList<Double>();
+        int x = 3;
+        for (int i = 0; i < 1000; i++) {
+            double startTime = System.currentTimeMillis();
+            naivePolynomialEvaluationAlgorithm(poly, x);
+            double endTime = System.currentTimeMillis();
+            times.add(endTime - startTime);
+        }
+        Double averageTime = times.stream().mapToDouble(d -> d).average().orElse(0.0);
+        System.out.println("Average time: " + averageTime);
+    }
 
+    public static double naivePolynomialEvaluationAlgorithm(double[] poly, double x) {
+        double result = 0;
+        for (int i = 0; i < poly.length; i++) {
+            result = result + poly[i] * Math.pow(3, i);
+        }
+        return result;
     }
 }
