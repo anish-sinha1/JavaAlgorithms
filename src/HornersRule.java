1d3a6555d7a289a3c99d340b61804b6134ab2bbf
Anish Sinha <anishsinha0128@gmail.com>
2021-08-23 15:20:19 -0400

    Modified Horner's rule algorithm

--- src/HornersRule.java
+++ src/HornersRule.java
@@ -1,15 +1,18 @@
 
 public class HornersRule {
     public static void main(String[] args) {
-        int[] poly = {2,-6,2,-1};
+        int[] poly = { -1, 2, -6, 2 };
         int x = 3;
         int n = poly.length;
         System.out.printf("The value of the polynomial at x=%d is %d", x, horner(poly, n, x));
-        
+
     }
-    public static int horner(int poly[], int n, int x){
+
+    // Polynomial evaluation
+    public static int horner(int poly[], int n, int x) {
         int result = 0;
-        for(int i = 0; i < n; i++) result = result*x+poly[i];
+        for (int i = poly.length; i > 0; i--)
+            result = poly[i - 1] + result * x;
         return result;
     }
 }
---
+++ src/NaivePolynomial.java
@@ -0,0 +1,5 @@
+public class NaivePolynomial {
+    public static void main(String[] args) {
+
+    }
+}
